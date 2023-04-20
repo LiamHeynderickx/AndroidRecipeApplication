436ad16e0678ced1cc27d584074d17736b995262
liamh <liam.heynderickx@student.kuleuven.be>
2023-04-20 21:19:24 +0200

    21:19    20/04/2022 shell of login

--- app/src/main/java/be/kuleuven/gt/project_recipe/CreateNewAccount.java
+++ app/src/main/java/be/kuleuven/gt/project_recipe/CreateNewAccount.java
@@ -38,6 +38,8 @@ public class CreateNewAccount extends AppCompatActivity {
         btnContinue.setVisibility(View.VISIBLE);
         lblAccountCreated.setVisibility(View.VISIBLE);
 
+
+
     }
 
 }
--- app/src/main/java/be/kuleuven/gt/project_recipe/Login.java
+++ app/src/main/java/be/kuleuven/gt/project_recipe/Login.java
@@ -6,23 +6,55 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
     private Button btnCreateNewAccount;
+    private TextView txtUsername;
+    private TextView txtPassword;
+    private String username;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         btnCreateNewAccount = (Button) findViewById(R.id.btnCreateNewAccount);
+        txtUsername = (TextView) findViewById(R.id.txtUsername);
+        txtPassword = (TextView) findViewById(R.id.txtPassword);
     }
 
     public void onBtnCreateNewAccount_Clicked (View Caller){
 
+
         Intent intent = new Intent(this, CreateNewAccount.class);
         startActivity(intent);
         finish();
+
+    }
+
+    public void onBtnLogin_Clicked(View Caller){
+
+        //Check details: (need to integrate databease)
+        username = txtUsername.getText().toString();
+        password = txtPassword.getText().toString();
+
+        if(username.equals("liam") || username.equals("matteo")){
+            if(password.equals("12345")){
+                finish();
+            }
+            else{
+                txtPassword.setText("");
+                txtPassword.setHint("Password Incorrect");
+            }
+        }
+        else{
+            txtUsername.setText("");
+            txtPassword.setText("");
+            txtUsername.setHint("Username Incorrect");
+            txtPassword.setHint("Enter Password");
+        }
 
     }
 
