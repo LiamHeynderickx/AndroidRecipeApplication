7dd0ec7efea90ad4c1a8ac2b3772b904db5a5037
liamh <liam.heynderickx@student.kuleuven.be>
2023-04-25 13:35:07 +0200

    changed action bar to a menu

--- app/src/main/java/be/kuleuven/gt/project_recipe/Help.java
+++ app/src/main/java/be/kuleuven/gt/project_recipe/Help.java
@@ -3,20 +3,53 @@ package be.kuleuven.gt.project_recipe;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class Help extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-//        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-//        getSupportActionBar().setDisplayShowCustomEnabled(true);
-////        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("319f95"));
-////        getSupportActionBar().setBackgroundDrawable(colorDrawable);  //couldnt find a colour i like
-//        getSupportActionBar().setCustomView(R.layout.custom_action_bar_layout);
-
         setContentView(R.layout.activity_help);
     }
+
+    @Override //copy to each new activity
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = new MenuInflater(this);
+        inflater.inflate(R.menu.menu_main_screen, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    private Intent menuOption; // copy to each activity
+    @Override // copy to each activity
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menuSettings:
+                menuOption = new Intent(this, Settings.class);
+                startActivity(menuOption);
+                return true;
+            case R.id.menuHelp:
+                menuOption = new Intent(this, Help.class);
+                startActivity(menuOption);
+                return true;
+            case R.id.menuLogout:
+                menuOption = new Intent(this, Login.class);
+                startActivity(menuOption);
+                finish();
+                return true;
+            case R.id.menuHome:
+                menuOption = new Intent(this, MainActivity.class);
+                startActivity(menuOption);
+                finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
--- app/src/main/java/be/kuleuven/gt/project_recipe/Settings.java
+++ app/src/main/java/be/kuleuven/gt/project_recipe/Settings.java
@@ -2,7 +2,11 @@ package be.kuleuven.gt.project_recipe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class Settings extends AppCompatActivity {
 
@@ -11,4 +15,40 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
     }
+
+    @Override //copy to each new activity
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = new MenuInflater(this);
+        inflater.inflate(R.menu.menu_main_screen, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    private Intent menuOption; // copy to each activity
+    @Override // copy to each activity
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menuSettings:
+                menuOption = new Intent(this, Settings.class);
+                startActivity(menuOption);
+                return true;
+            case R.id.menuHelp:
+                menuOption = new Intent(this, Help.class);
+                startActivity(menuOption);
+                return true;
+            case R.id.menuLogout:
+                menuOption = new Intent(this, Login.class);
+                startActivity(menuOption);
+                finish();
+                return true;
+            case R.id.menuHome:
+                menuOption = new Intent(this, MainActivity.class);
+                startActivity(menuOption);
+                finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
