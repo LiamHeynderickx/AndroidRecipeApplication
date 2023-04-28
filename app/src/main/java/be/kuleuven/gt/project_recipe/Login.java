db8a1640c51b75d06418e795a56bd4ed4d36b27b
Mateo Sakr <mateo.sakr@student.kuleuven.be>
2023-04-28 14:57:23 +0200

    test3

--- app/src/main/java/be/kuleuven/gt/project_recipe/Login.java
+++ app/src/main/java/be/kuleuven/gt/project_recipe/Login.java
@@ -7,6 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class Login extends AppCompatActivity {
 
@@ -15,6 +29,16 @@ public class Login extends AppCompatActivity {
     private TextView txtPassword;
     private String username;
     private String password;
+    private ArrayList<String> listUsernames = new ArrayList<>();
+    private ArrayList<String> listPasswords = new ArrayList<>();
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +47,8 @@ public class Login extends AppCompatActivity {
         btnCreateNewAccount = (Button) findViewById(R.id.btnCreateNewAccount);
         txtUsername = (TextView) findViewById(R.id.txtUsername);
         txtPassword = (TextView) findViewById(R.id.txtPassword);
+        retrieveLoginData();
+
     }
 
     public void onBtnCreateNewAccount_Clicked (View Caller){
@@ -33,29 +59,69 @@ public class Login extends AppCompatActivity {
         finish();
 
     }
+    private static final String QUEUE_URL = "https://studev.groept.be/api/a22pt415/All_Data";
+
+
+    private void retrieveLoginData(){
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonArrayRequest queueRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                QUEUE_URL,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject curObject = response.getJSONObject(i);
+                                String Username = curObject.getString("Username");
+                                String Password = curObject.getString("Password");
+                                listUsernames.add(Username);
+                                listPasswords.add(Password);
+
+                               }
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(
+                                Login.this,
+                                "Unable to communicate with the server",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+        requestQueue.add(queueRequest);
+    }
 
     public void onBtnLogin_Clicked(View Caller){
 
         //Check details: (need to integrate databease)
         username = txtUsername.getText().toString();
         password = txtPassword.getText().toString();
-
-        if(username.equals("liam") || username.equals("X")){
-            if(password.equals("X")){
-                Intent intent2 = new Intent(this, MainActivity.class);
-                startActivity(intent2);
-                finish();
+        for(int i=0;i< listPasswords.size();i++)
+        {
+            if(username.equals(listUsernames.get(i)) ){
+                if(password.equals(listPasswords.get(i))){
+                    Intent intent2 = new Intent(this, MainActivity.class);
+                    startActivity(intent2);
+                    finish();
+                }
+                else{
+                    txtPassword.setText("");
+                    txtPassword.setHint("Password Incorrect");
+                }
             }
             else{
+                txtUsername.setText("");
                 txtPassword.setText("");
-                txtPassword.setHint("Password Incorrect");
+                txtUsername.setHint("Username Incorrect");
+                txtPassword.setHint("Enter Password");
             }
-        }
-        else{
-            txtUsername.setText("");
-            txtPassword.setText("");
-            txtUsername.setHint("Username Incorrect");
-            txtPassword.setHint("Enter Password");
+
         }
 
 
