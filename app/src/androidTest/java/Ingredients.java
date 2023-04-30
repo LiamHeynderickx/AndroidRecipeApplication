24627efd7ef067b8fcf3c05cf0a8df8878acddad
Mateo Sakr <mateo.sakr@student.kuleuven.be>
2023-04-30 23:59:59 +0200

    teste ingredients1

---
+++ app/src/androidTest/java/Ingredients.java
@@ -0,0 +1,45 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.io.File;
+import java.util.Scanner;
+
+public class Ingredients {
+    public ArrayList<String> ingredientsList = new ArrayList<>();
+    public void populateList() throws FileNotFoundException {
+        try {
+                    // Create a new File object with the path to the file
+                    File file = new File("ingredients.txt");
+
+                    // Create a new Scanner object to read from the file
+                    Scanner scanner = new Scanner(file);
+
+                    // Read each line of the file
+                    while (scanner.hasNextLine()) {
+                        String line = scanner.nextLine();
+                        // Do something with the line (e.g. print it)
+                       ingredientsList.add(line);
+                    }
+
+                    // Close the scanner
+                    scanner.close();
+                } catch (FileNotFoundException e) {
+                    // Handle the FileNotFoundException
+                    e.printStackTrace();
+                }
+            }
+    public ArrayList<String> getIngredientsList()
+    {
+        return ingredientsList;
+    }
+    public void printTest()
+    {
+        for(int i=0; i<6;i++)
+        {
+            System.out.println(ingredientsList.get(i));
+        }
+    }
+
+}
+
+
+
