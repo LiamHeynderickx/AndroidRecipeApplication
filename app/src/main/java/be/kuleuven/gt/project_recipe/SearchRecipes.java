84ca9088ffcd617b87662ef4ab088fb563c67173
liamh <liam.heynderickx@student.kuleuven.be>
2023-05-06 14:08:28 +0200

    keyboard go down

--- app/src/main/java/be/kuleuven/gt/project_recipe/SearchRecipes.java
+++ app/src/main/java/be/kuleuven/gt/project_recipe/SearchRecipes.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -41,7 +43,7 @@ public class SearchRecipes extends AppCompatActivity {
     private ApiManager api = new ApiManager();
     private MyAdapter myAdapter = new MyAdapter(this, recipeList);
 
-    String diet;
+    String diet = "";
 
 
     private TextView txtRecipeName;
@@ -72,6 +74,13 @@ public class SearchRecipes extends AppCompatActivity {
 
 
     public void onBtnSearchByName_Clicked(View caller){
+
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+
 
         recyclerView.removeAllViews();
 //        getRecipes();
