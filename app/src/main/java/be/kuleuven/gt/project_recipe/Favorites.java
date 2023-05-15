090f60c8d68015f37463c77442f299afeffc18bf
Mateo Sakr <mateo.sakr@student.kuleuven.be>
2023-05-15 14:52:56 +0200

    getFavorite

--- app/src/main/java/be/kuleuven/gt/project_recipe/Favorites.java
+++ app/src/main/java/be/kuleuven/gt/project_recipe/Favorites.java
@@ -9,6 +9,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -16,25 +28,25 @@ public class Favorites extends AppCompatActivity implements RecyclerViewInterfac
 
 
     private ArrayList<RecipeInformation> recipeList = new ArrayList<>();
+    private ArrayList<String>recipeIdList = new ArrayList<>();
     private ApiManager api = new ApiManager();
     private MyAdapter myAdapter = new MyAdapter(this, recipeList, this);
     private RecyclerView recyclerView;
+    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
         setTitle("Favorites");
-
+        Intent intent = getIntent();
+        username = intent.getStringExtra("USERNAME");
         recyclerView = (RecyclerView) findViewById(R.id.recyclerViewFavorites);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         recyclerView.setAdapter(myAdapter);
-
-        getFavorites(); //connection to database
-
-
-        recyclerView.setAdapter(new MyAdapter(getApplicationContext(),recipeList, this));
+        getFavorites();
+      //  recyclerView.setAdapter(new MyAdapter(getApplicationContext(),recipeList, this));
 
     }
 
@@ -75,12 +87,41 @@ public class Favorites extends AppCompatActivity implements RecyclerViewInterfac
 
     public void getFavorites(){
         //connect to database of favorites
+        String QUEUE_URL = "https://studev.groept.be/api/a22pt409/getFavorites/"+username;
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonArrayRequest queueRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                QUEUE_URL,
+                null,
+                response -> {
+                    try {
+                        for (int i = 0; i < response.length(); i++) {
+                            JSONObject curObject = response.getJSONObject(i);
+                            String currRecipeID = curObject.getString("recipeId");
+                            String currRecipeName = curObject.getString("recipeName");
+                            RecipeInformation ri = new RecipeInformation();
+                            ri.setRecipeName(currRecipeName);
+                            ri.setRecipeID(currRecipeID);
+                            recipeList.add(ri);
 
+
+                        }
+                        recyclerView.setAdapter(new MyAdapter(getApplicationContext(),recipeList, this));
+
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                },
+                error -> Toast.makeText(
+                        Favorites.this,
+                        "Unable to communicate with the server",
+                        Toast.LENGTH_LONG).show());
+        requestQueue.add(queueRequest);
         //temporary code:
-        RecipeInformation ri = new RecipeInformation();
-        ri.setRecipeName("Easy Homemade Rice and Beans");
-        ri.setRecipeID("716627");
-        recipeList.add(ri);
+//        RecipeInformation ri = new RecipeInformation();
+//        ri.setRecipeName("Easy Homemade Rice and Beans");
+//        ri.setRecipeID("716627");
+//        recipeList.add(ri);
     }
 
     @Override
