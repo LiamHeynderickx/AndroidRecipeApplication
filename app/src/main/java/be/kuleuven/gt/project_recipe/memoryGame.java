6434ae1b8bd78104f274704c6ec5a78b1a91579b
liamh <liam.heynderickx@student.kuleuven.be>
2023-05-22 13:01:40 +0200

    button Star done

--- app/src/main/java/be/kuleuven/gt/project_recipe/memoryGame.java
+++ app/src/main/java/be/kuleuven/gt/project_recipe/memoryGame.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class memoryGame extends AppCompatActivity {
     private ImageButton btnMeat;
@@ -18,6 +21,7 @@ public class memoryGame extends AppCompatActivity {
     private ImageButton btnRice;
     private ImageButton btnTomato;
     private ImageButton btnPasta;
+    private Button btnStart;
 
     private int round = 1;
 
@@ -34,6 +38,7 @@ public class memoryGame extends AppCompatActivity {
         btnRice = (ImageButton)findViewById(R.id.btnRice);
         btnTomato = (ImageButton)findViewById(R.id.btnTomato);
         btnPasta = (ImageButton)findViewById(R.id.btnPasta);
+        btnStart = (Button) findViewById(R.id.btnStart);
 
     }
 
@@ -76,44 +81,80 @@ public class memoryGame extends AppCompatActivity {
 
         createRandomArray();
 
+        displayImageSequence();
 
+        btnStart.setVisibility(View.GONE);
 
         round++;
+    }
+
+    private void displayImageSequence() {
+
+
+
     }
 
     private void createRandomArray() {
 
         for(int loop1 = 0; loop1 < round; loop1++){
-            int random =     (int) Math.random()*(4 - 0 + 1);
+
+            int randomNum = ThreadLocalRandom.current().nextInt(0, 4 + 1);
+
+            switch (randomNum){
+                case 0:
+                    Meat m = new Meat();
+                    ingredientOrder.add(m);
+                    break;
+                case 1:
+                    Lettuce l = new Lettuce();
+                    ingredientOrder.add(l);
+                    break;
+                case 2:
+                    rice r = new rice();
+                    ingredientOrder.add(r);
+                    break;
+                case 3:
+                    Tomato t = new Tomato();
+                    ingredientOrder.add(t);
+                    break;
+                case 4:
+                    Pasta p = new Pasta();
+                    ingredientOrder.add(p);
+                    break;
+                default:
+                    Log.d("ERROR ++++++++++", randomNum+" num <--");
+                    break;
+            }
+
         }
 
     }
 
-    public void onBtn_Meat_Clicked(View Caller)
+    public void onBtn_Meat_Clicked(View Caller) //0
     {
         IngredientType meat = new Meat();
         selectedOrder.add(meat);
         checkIfEqual();
     }
-    public void onBtn_Lettuce_Clicked(View Caller)
+    public void onBtn_Lettuce_Clicked(View Caller) //1
     {
         IngredientType lettuce = new Lettuce();
         selectedOrder.add(lettuce);
         checkIfEqual();
     }
-    public void onBtn_Rice_Clicked(View Caller)
+    public void onBtn_Rice_Clicked(View Caller) //2
     {
         IngredientType rice = new rice();
         selectedOrder.add(rice);
         checkIfEqual();
     }
-    public void onBtn_Tomato_Clicked(View Caller)
+    public void onBtn_Tomato_Clicked(View Caller) //3
     {
         IngredientType tomato = new Tomato();
         selectedOrder.add(tomato);
         checkIfEqual();
     }
-    public void onBtn_Pasta_Clicked(View Caller)
+    public void onBtn_Pasta_Clicked(View Caller) //4
     {
         IngredientType pasta = new Pasta();
         selectedOrder.add(pasta);
